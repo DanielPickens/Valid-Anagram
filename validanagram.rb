4b3e22404f5270778c5f3886807c01b2594ee261
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-11-15 02:20:35 -0700

    Update validanagram.rb

--- validanagram.rb
+++ validanagram.rb
@@ -1,16 +1,17 @@
-#time complexity: O(n)
-#Space complexity: O(1)
-
 #We can first check to see if the strings have the same length, 
-#then we iterate through one string and check to make sure the other string has that character,
+#then we can iterate through one string and check to make sure the other string has that character,
 #and if it does, then we need to change it.
 # If we exit the loop it must be true. 
 
 
+#time complexity: O(n)
+#Space complexity: O(1)
+
+
 def is_anagram(s, t)
- return false unless s.length == t.length  
+ return false unless s.length == t.length  #If the index of string t is 0 then it is not match between both strings, as we return false.
   soccurs = s.chars.tally
-  toccurs = t.chars.tally
+  toccurs = t.chars.tally  
    
   return soccurs == toccurs
 end  
